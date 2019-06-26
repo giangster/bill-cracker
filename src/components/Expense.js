2a6246b46aa64347c310e5c52c99c0338297dc12
Giang Nguyen <thuygiang152@gmail.com>
2019-06-26 22:59:16 +0300

    balance calculating updated

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 
 export default class Expense extends Component {
-  shareCalculate = props => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sharePerPerson: 0
+    };
+  }
+
+  componentDidMount = () => {
     var total = 0;
-    for (let i = 0; i < props.data.length; i++) {
-      total = total + props.data[i].money;
-    }
-    var sharePerPerson = total / 4;
-    let balanceGeneral = [];
-    for (let i = 0; i < props.data.length; i++) {
-      let balance = props.data[i].money - sharePerPerson;
-      balanceGeneral.push(balance);
-    }
-    return balanceGeneral;
+
+    this.props.data.forEach(function(item) {
+      total = total + parseFloat(item.money);
+    });
+    var share = total / this.props.data.length;
+
+    // console.log(share);
+    this.setState({ sharePerPerson: share });
+
+    // console.log(this.state.sharePerPerson);
+    var balanceGeneral = [];
+    this.props.data.forEach(function(item) {
+      balanceGeneral.push(parseFloat(item.money) - share);
+    });
+
+    console.log(balanceGeneral);
   };
+
   render() {
     return (
       <div>
-        <p>Here's the calculation.</p>
+        <p>Here's the calculation:</p>
+        <p>Each person's share is {this.state.sharePerPerson}</p>
+        <p>Therefore</p>
       </div>
     );
   }
