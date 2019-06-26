00ee9003392090e0dea5020bfd66e333aac3ee32
Giang Nguyen <thuygiang152@gmail.com>
2019-06-26 14:39:21 +0300

    expense component created, share per person functionality created

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Expense = props => {
-  return (
-    <div>
-      <p>Here's the calculation.</p>
-    </div>
-  );
-};
-
-export default Expense;
+export default class Expense extends Component {
+  shareCalculate = props => {
+    var total = 0;
+    for (let i = 0; i < props.data.length; i++) {
+      total = total + props.data[i].money;
+    }
+    var sharePerPerson = total / 4;
+    let balanceGeneral = [];
+    for (let i = 0; i < props.data.length; i++) {
+      let balance = props.data[i].money - sharePerPerson;
+      balanceGeneral.push(balance);
+    }
+    return balanceGeneral;
+  };
+  render() {
+    return (
+      <div>
+        <p>Here's the calculation.</p>
+      </div>
+    );
+  }
+}
