163599ca5d0320b4ad2d9b652c0f6fa19a0c124f
Giang Nguyen <thuygiang152@gmail.com>
2019-06-27 22:50:01 +0300

    calculation basic- done

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -33,22 +33,28 @@ export default class Expense extends Component {
     let i = 0;
     let j = participant.length - 1;
     let debt;
-
+    let result = "";
     while (i < j) {
       debt = Math.min(Math.abs(balanceGeneral[i], balanceGeneral[j]));
-      console.log();
+      console.log(`${participant[i]} owes ${participant[j]} ${debt} euroes`);
+      result = `${result}
+      ${participant[i]} owes ${participant[j]} ${debt} euroes
+      `;
+
       balanceGeneral[i] += debt;
       balanceGeneral[j] -= debt;
       balanceGeneral[i] === 0 ? i++ : j--;
     }
+    this.setState({ result: result });
   };
 
   render() {
     return (
       <div>
         <p>Here's the calculation:</p>
-        <p>Each person's share is {this.state.sharePerPerson}</p>
-        <p>Therefore</p>
+        <p>Each person's share is {this.state.sharePerPerson} euroes.</p>
+        <p>Therefore:</p>
+        <p>{this.state.result}</p>
       </div>
     );
   }
