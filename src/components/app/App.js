6991c05f6747de0b82bd1fa3fb3143f4bb1e6453
Giang Nguyen <thuygiang152@gmail.com>
2019-08-03 15:14:47 +0300

    Restructure StartPage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import StartPage from "../StartPage";
+import StartPage from "../StartPage/StartPage";
 import { Provider } from "react-redux";
 import store from "../../store";
 
