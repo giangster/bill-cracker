c1ddb957551c35becec8799174a5ab7f3f27be92
Le <dean.le@f-secure.com>
2019-08-08 10:57:31 +0300

    fix import error

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import participantReducer from "./participantListReducer";
+import participantReducer from "./participantList";
 
 export default combineReducers({
   participantList: participantReducer
