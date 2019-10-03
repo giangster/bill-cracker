b045c0c4bc97953c84878a3bd114764f10225863
Giang Nguyen <thuygiang152@gmail.com>
2019-10-03 20:50:59 +0300

    Calculate share unit test, 1st test

--- src/components/Result/Result.test.js
+++ src/components/Result/Result.test.js
@@ -1,20 +1,60 @@
-import { calculateAverage } from "./Result";
+import { calculateShare, calculateAverage } from "./Result";
 
-test("Should calculate average correctly", () => {
-  let participants = [
-    {
-      name: "",
-      money: 3
-    },
-    {
-      name: "",
-      money: 4
-    },
-    {
-      name: "",
-      money: 5
-    }
-  ];
+describe("Calculate average tests", () => {
+  test("Should calculate average correctly", () => {
+    let participants = [
+      {
+        participantName: "",
+        money: 3
+      },
+      {
+        participantName: "",
+        money: 4
+      },
+      {
+        participantName: "",
+        money: 5
+      }
+    ];
+    expect(calculateAverage(participants)).toBe(4);
+  });
 
-  expect(calculateAverage(participants)).toBe(4);
+  test("Should return NaN", () => {
+    let participants = [
+      {
+        participantName: "",
+        money: 3
+      },
+      {
+        participantName: "",
+        money: "abc"
+      }
+    ];
+
+    expect(calculateAverage(participants)).toBeNaN();
+  });
+});
+
+describe("Calculate share tests", () => {
+  test("Should calculate share accurately", () => {
+    let participants = [
+      {
+        participantName: "Dog",
+        money: 3
+      },
+      {
+        participantName: "Cat",
+        money: 4
+      },
+      {
+        participantName: "Duck",
+        money: 5
+      }
+    ];
+
+    expect(calculateShare(participants)).toStrictEqual([
+      "Dog owes Duck 1.00 euros.",
+      "Cat owes Duck 0.00 euros."
+    ]);
+  });
 });
