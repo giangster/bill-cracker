998acf348c05f0b12c8687c01e91c80b25686fe9
Giang Nguyen <thuygiang152@gmail.com>
2019-10-03 21:05:00 +0300

    Reducing redundancy by using beforeEach()

--- src/components/Result/Result.test.js
+++ src/components/Result/Result.test.js
@@ -1,8 +1,9 @@
 import { calculateShare, calculateAverage } from "./Result";
 
-describe("Calculate average tests", () => {
-  test("Should calculate average correctly", () => {
-    let participants = [
+describe("Result.js test suite", () => {
+  let participants = [];
+  beforeEach(() => {
+    participants = [
       {
         participantName: "",
         money: 3
@@ -16,44 +17,37 @@ describe("Calculate average tests", () => {
         money: 5
       }
     ];
-    expect(calculateAverage(participants)).toBe(4);
+  });
+  describe("Calculate average tests", () => {
+    test("Should calculate average correctly", () => {
+      expect(calculateAverage(participants)).toBe(4);
+    });
+
+    test("Should return NaN", () => {
+      participants[1].money = "abc";
+      expect(calculateAverage(participants)).toBeNaN();
+    });
   });
 
-  test("Should return NaN", () => {
-    let participants = [
-      {
-        participantName: "",
-        money: 3
-      },
-      {
-        participantName: "",
-        money: "abc"
-      }
-    ];
+  describe("Calculate share tests", () => {
+    test("Should calculate share accurately", () => {
+      participants[0].participantName = "Dog";
+      participants[1].participantName = "Cat";
+      participants[2].participantName = "Duck";
+      participants[2].money = 8;
+      expect(calculateShare(participants)).toStrictEqual([
+        "Dog owes Duck 2.00 euros.",
+        "Cat owes Duck 1.00 euros."
+      ]);
+    });
 
-    expect(calculateAverage(participants)).toBeNaN();
-  });
-});
-
-describe("Calculate share tests", () => {
-  test("Should calculate share accurately", () => {
-    let participants = [
-      {
-        participantName: "Dog",
-        money: 3
-      },
-      {
-        participantName: "Cat",
-        money: 4
-      },
-      {
-        participantName: "Duck",
-        money: 5
-      }
-    ];
-
-    expect(calculateShare(participants)).toStrictEqual([
-      "Dog owes Duck 1.00 euros."
-    ]);
+    test("Should not print zero debt", () => {
+      participants[0].participantName = "Dog";
+      participants[1].participantName = "Cat";
+      participants[2].participantName = "Duck";
+      expect(calculateShare(participants)).toStrictEqual([
+        "Dog owes Duck 1.00 euros."
+      ]);
+    });
   });
 });
