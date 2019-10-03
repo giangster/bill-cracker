390f128e3746b25740bbe3a992aedebd79f0b825
Giang Nguyen <thuygiang152@gmail.com>
2019-10-03 21:16:15 +0300

    Unit tests completed

--- src/components/Result/Result.test.js
+++ src/components/Result/Result.test.js
@@ -30,21 +30,36 @@ describe("Result.js test suite", () => {
   });
 
   describe("Calculate share tests", () => {
-    test("Should calculate share accurately", () => {
+    beforeEach(() => {
       participants[0].participantName = "Dog";
       participants[1].participantName = "Cat";
       participants[2].participantName = "Duck";
+    });
+    //Divide a unit test into three smaller parts: establishment, execution, and assertion
+    test("Should calculate share accurately", () => {
       participants[2].money = 8;
-      expect(calculateShare(participants)).toStrictEqual([
+
+      let result = calculateShare(participants);
+
+      expect(result).toStrictEqual([
         "Dog owes Duck 2.00 euros.",
         "Cat owes Duck 1.00 euros."
       ]);
     });
 
+    test("Should calculate complex input accurately", () => {
+      participants.push({ participantName: "Rabbit", money: 1520 });
+
+      let result = calculateShare(participants);
+
+      expect(result).toStrictEqual([
+        "Dog owes Rabbit 380.00 euros.",
+        "Cat owes Rabbit 379.00 euros.",
+        "Duck owes Rabbit 378.00 euros."
+      ]);
+    });
+
     test("Should not print zero debt", () => {
-      participants[0].participantName = "Dog";
-      participants[1].participantName = "Cat";
-      participants[2].participantName = "Duck";
       expect(calculateShare(participants)).toStrictEqual([
         "Dog owes Duck 1.00 euros."
       ]);
